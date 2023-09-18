a71e3a9c331b14e6d388e62fead3e2c5e8b319e6
BRIJESH6063 <=>
2023-09-19 00:05:51 +0530

    tic-tac-toe commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tictactoe from './component/t2t/Tictactoe' ;
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
